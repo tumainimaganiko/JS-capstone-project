ffd7e9e76dff9f4fabfd8e0ebcf94c48df387842
tumainimaganiko <tumainimaganiko@gmail.com>
2023-05-23 20:37:00 +0300

    Use modules to display items

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 import './styles/style.css';
+import { retrieveItems } from './modules/display.js';
+
+for(let i =1; i < 7; i++){
+    const ans = await retrieveItems(i);
+
+    const div = document.createElement('div');
+    div.className ='container';
+    const img = document.createElement('img');
+    img.src = ans.image.medium;
+    const span = document.createElement('span');
+    span.innerHTML = `${ans.name}`;
+    const comment = document.createElement('a')
+    comment.innerHTML = 'Comments';
+    comment.href = ''
+    const reservations = document.createElement('a')
+    reservations.innerHTML = 'Reservations';
+    reservations.href = '';
+
+    div.appendChild(img);
+    div.appendChild(span);
+    div.appendChild(comment);
+    div.appendChild(reservations);
+
+    const main = document.querySelector('main');
+    main.appendChild(div);
+}
