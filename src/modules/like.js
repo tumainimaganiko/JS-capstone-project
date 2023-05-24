335d06aa14f294354ea1f25c754d098e17906749
tumainimaganiko <tumainimaganiko@gmail.com>
2023-05-24 22:48:37 +0300

    Update Like modules

--- src/modules/like.js
+++ src/modules/like.js
@@ -1,24 +1,25 @@
 const likeBtn = async (likeId) => {
-    const like = await fetch (`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/lrcVZB2tmQHgSmgF6kle/likes/`, {
-        method: 'POST',
-        body: JSON.stringify({
-            item_id: `${likeId}`
-        }),
-        headers: {
-            'content-type': 'application/json; charset=UTF-8',
-        },
-    })
-}
+  await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/76pKaDmDMWX5au9eSHVx/likes/', {
+    method: 'POST',
+    body: JSON.stringify({
+      item_id: `${likeId}`,
+    }),
+    headers: {
+      'content-type': 'application/json; charset=UTF-8',
+    },
+  });
+};
+
 const displayLike = async () => {
-    const like = await fetch (`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/lrcVZB2tmQHgSmgF6kle/likes/`, {
-        method: 'GET',
-        headers: {
-            'content-type': 'application/json; charset=UTF-8',
-        },
-    });
+  const like = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/76pKaDmDMWX5au9eSHVx/likes/', {
+    method: 'GET',
+    headers: {
+      'content-type': 'application/json; charset=UTF-8',
+    },
+  });
 
-    const result = await like.json();
-    return result;
-}
+  const result = await like.json();
+  return result;
+};
 
-export {likeBtn, displayLike};
+export { likeBtn, displayLike };
